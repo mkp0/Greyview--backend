08850fe299646e492b4acc636d262f71c903852d
mkp0 <shadymasum@gmail.com>
2022-02-05 23:23:52 +0530

    intialise

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv/config");
+const { DB_CONNECTION } = require("./utils/DB_CONNECTION");
+DB_CONNECTION(); // database connection
+
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "GREY GANG BACKEND",
+        ip_address: req.connection.remoteAddress,
+    });
+});
+
+
+const PORT = process.env.PORT || 3800;
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
