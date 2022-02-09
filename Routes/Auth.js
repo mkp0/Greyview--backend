47f26926476ac84938843762dcb25857f17b78e1
mkp0 <shadymasum@gmail.com>
2022-02-10 01:45:32 +0530

    deployed bug fixed

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,12 +1,43 @@
 const { Router } = require("express");
-const { LOGIN, SIGN_IN } = require("../controllers/AuthController");
+// const { SIGN_IN } = require("../controllers/AuthController");
 const auth = require("../Middlewares/Auth");
 const router = Router();
+const jwt = require("jsonwebtoken");
 
-router.get("/", (req, res) => {
-  res.send("Hello world");
+router.post("/login", (req, res) => {
+  const { id, password } = req.body;
+  if (!id || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  if (
+    id !== process.env.ADMIN_IDD ||
+    password !== process.env.ADMIN_PASSWORDD
+  ) {
+    return res.status(400).json({ msg: "Wrong credential" });
+  }
+
+  jwt.sign(
+    { id: id, password: password },
+    process.env.JWT_SECRET,
+    { expiresIn: 36000 },
+    (err, token) => {
+      if (err) {
+        return res.json({
+          err: "error",
+        });
+      }
+      return res.json({
+        token,
+        status: "success",
+      });
+    }
+  );
 });
-router.post("/login", LOGIN);
-router.get("/signin", auth, SIGN_IN);
+router.get("/signin", auth, (req, res) => {
+  res.send({
+    res: "success",
+  });
+});
 
 module.exports = router;
